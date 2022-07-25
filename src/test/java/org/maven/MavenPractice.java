0b5fdd9c9e766b11b24c94bfd86553a44811d28b
vikkitn <lvignesh2697@gmail.com>
2022-07-25 13:06:02 +0530

    first change

---
+++ src/test/java/org/maven/MavenPractice.java
@@ -0,0 +1,32 @@
+package org.maven;
+
+import org.testng.annotations.Test;
+
+public class MavenPractice {
+	@Test
+	public void Sample1()
+	{
+		System.out.println("MavenPratice1");
+	}
+	@Test
+	public void Sample2()
+	{
+		System.out.println("MavenPratice2");
+	}
+	@Test
+	public void Sample3()
+	{
+		System.out.println("MavenPratice3");
+		System.out.println("MavenPratice4");
+	}
+	public void Sample4()
+	{
+		System.out.println("MavenPratice5");
+	}
+	public void Sample5()
+	{
+		System.out.println("Project Success");
+	}
+
+
+}
---
+++ src/test/java/org/maven/MavenProject2.java
@@ -0,0 +1,21 @@
+package org.maven;
+
+import org.testng.annotations.Test;
+
+public class MavenProject2 {
+	@Test
+	public void Demo1()
+	{
+		System.out.println("DemoMavenPratice1");
+	}
+	@Test
+	public void Demo2()
+	{
+		System.out.println("DemoMavenPratice2");
+	}
+	@Test
+	public void Demo3()
+	{
+		System.out.println("DemoMavenPratice3");
+	}
+}
